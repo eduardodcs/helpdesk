ea8813c15c08bdda41a8308b1048fa998ca8c80a
EduardoSilva <eddcesilva@gmail.com>
2022-05-22 15:55:41 -0300

    Endpoint findById para tecnico

---
+++ src/main/java/br/edu/helpdesk/resources/TecnicoResource.java
@@ -0,0 +1,26 @@
+package br.edu.helpdesk.resources;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.edu.helpdesk.domain.Tecnico;
+import br.edu.helpdesk.services.TecnicoService;
+
+@RestController
+@RequestMapping(value = "/tecnicos")
+public class TecnicoResource {
+	
+	@Autowired
+	private TecnicoService tecnicoService;
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Tecnico> findById(@PathVariable Integer id) {
+		Tecnico tecnico = this.tecnicoService.findById(id);
+		return ResponseEntity.ok().body(tecnico);
+	}
+
+}
